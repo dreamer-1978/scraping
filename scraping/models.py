c25a8418c517f955227d4938dce96dcd44f24897
dreamer-1978 <dreamer-1978@mail.ru>
2022-06-07 00:01:21 +0300

    create two models

--- scraping/models.py
+++ scraping/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class City(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Название Города')
-    slug = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, verbose_name='Название Города', unique=True)
+    slug = models.CharField(max_length=25, unique=True, blank=True)
 
     class Meta:
         verbose_name = 'Города'
@@ -11,3 +12,29 @@ class City(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = str(self.name)[:10]
+        super().save(*args, **kwargs)
+
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Языки программирования', unique=True)
+    slug = models.CharField(max_length=25, unique=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = str(self.name)[:10]
+        super().save(*args, **kwargs)
+
+
+
