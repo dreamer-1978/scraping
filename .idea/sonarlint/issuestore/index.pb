dd1e90c8070c0d2707b62cef6529a4c6d199dc85
dreamer-1978 <dreamer-1978@mail.ru>
2022-06-06 20:39:13 +0300

    create_model

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-L
-scraping_service/settings.py,e/d/ed8ea3771dc2d6d067ab198b9cc4226c6b4d2f48
--- scraping/models.py
+++ scraping/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название Города')
+    slug = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        verbose_name = 'Города'
+        verbose_name_plural = 'Название Город'
+
+    def __str__(self):
+        return self.name
